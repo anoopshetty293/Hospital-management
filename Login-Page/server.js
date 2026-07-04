701c9498d52985e275a244e8096b6e90ebce70b3
ANobodyFreelancer <sharmaparash464@gmail.com>
2026-07-04 15:36:38 +0530

    The HTTP request-Response Handling File

---
+++ Login-Page/server.js
@@ -0,0 +1,96 @@
+const http = require("http");
+const fs = require("fs");
+const querystring = require("querystring");
+const filenameHTML ='frontendHTML.html';
+const filenameCSS ='frontendCSS.css';
+
+const server = http.createServer((req, res)=>{
+
+    switch(req.url){
+
+        case "/":
+            fs.readFile(filenameHTML, (err, data)=>{
+                if(err){
+                res.end(err);
+                }
+                else{   
+                res.end(data);
+                }
+            });
+        break;
+
+        case "/frontendCSS.css":
+            fs.readFile(filenameCSS, (err, data)=>{
+                if(err){
+                res.end(err);
+                }
+                else{
+                res.end(data);
+                }
+            });
+        break;
+
+        case "/LEMONMILK-Bold.otf":
+            fs.readFile("LEMONMILK-Bold.otf", (err, data)=>{
+                if(err){
+                res.end(err);
+                }
+                else{
+                res.end(data);
+                }
+            });
+        break;
+
+        case "/LEMONMILK-Light.otf":
+            fs.readFile("LEMONMILK-Light.otf", (err, data)=>{
+                if(err){
+                res.end(err);
+                }
+                else{
+                res.end(data);
+                }
+            });
+        break;
+
+        case "/done":
+
+            //Data Parsing (Decrypting the user data coming from the the loginpage)
+            let body = ''; 
+            req.on('data', (chunk)=>{
+                body += chunk.toString();
+            });
+            req.on('end', ()=>{
+                const parsedData = querystring.parse(body);
+                const pool = require('./db'); 
+                
+                // Query to insert the user data into the Users table in the database
+                const query = {
+                  text: 'INSERT INTO Users(Username, Password1) VALUES($1, $2)',
+                  values: [parsedData.username, parsedData.password],
+                }
+                
+                pool.query(query) 
+                .then(result => { 
+                    console.log(result.rows); 
+                }) 
+                .catch(error => { 
+                    console.log(error); 
+                });
+                res.end("Data received successfully.");
+            });
+
+        break;
+
+
+        default:
+        res.end("404 Not Found.");
+    }
+
+});
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+server.listen(PORT, HOST, ()=>{
+    console.log(`Server running at http://${HOST}:${PORT}/`);
+});
